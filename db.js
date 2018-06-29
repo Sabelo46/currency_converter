27cc813727e525e37f3a0390987acb01bc52145b
sabelo46 <bellosamuel64@gmail.com>
2018-06-29 22:33:29 +0100

    Confirm caches of currency

--- db.js
+++ db.js
@@ -1,3 +1,4 @@
+import idb from 'idb';
 var dbPromise = idb.open('load-db',1,function(upgradeDb){
    if(!upgradeDb.objectStoreNames.contains('store')){
        upgradeDb.createObjectStore('store'); 
