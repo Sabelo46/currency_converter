1325ac6ccf793ea38025727abd97c94851fc7388
sabelo46 <bellosamuel64@gmail.com>
2018-06-29 18:07:37 +0100

    Confirming cache adding

--- sw.js
+++ sw.js
@@ -1,5 +1,35 @@
+var cacheName = 'v1';
+var cacheFiles = [
+                  'index.html',
+                  'ext.css',
+                  'im/a.jpg',
+                  'im/b.jpg',
+                  'im/c.jpg',
+                  'js/convert.js',
+                  'js/currenciesController.js',
+                  'node_modules/bootstrap/dist/css/bootstrap.css',
+                  'node_modules/bootstrap/dist/js/bootstrap.js',
+                  'node_modules/jquery/dist/jquery.js'
+                  ]
+self.addEventListener('install', function(event) {
+  // Perform install step
+  console.log("I'm ready to install for you");
+  console.log(event.request);
+  event.waitUntil(
+    caches.open(cacheName).then(function(cache){
+    	console.log('Adding files to cache');
+    	return cache.addAll(cacheFiles);
+    })
+  )
+});
+self.addEventListener('activated', function(event) {
+  // Perform install step
+  console.log("I'm currently activated for you..");
+  console.log(event.request);
+});
+
 self.addEventListener('fetch', function(event) {
   // Perform install step
-  console.log('Confirming if ready');
+  console.log("I'm ready to fetch for you..");
   console.log(event.request);
 });
