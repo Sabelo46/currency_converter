9d3acb715e69cddae1b1147353714ffb11ffde4f
sabelo46 <bellosamuel64@gmail.com>
2018-06-29 21:37:10 +0100

    Confirm caches of currency

--- sw.js
+++ sw.js
@@ -40,7 +40,7 @@ var cacheFiles = [
 
     self.addEventListener('fetch',function(event){
       event.respondWith(
-        caches.match(event.request).then(function(event){
+        caches.match(event.request).then(function(response){
           if(response){
             console.log('Found Service worker in cache',event.request.url);
             return response;
