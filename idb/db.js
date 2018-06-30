2051a28e272c06e10b9fc6e49033a08e8ad8b463
sabelo46 <bellosamuel64@gmail.com>
2018-06-30 21:31:22 +0100

    Confirm caches of currency v2

--- idb/db.js
+++ idb/db.js
@@ -1,39 +1,12 @@
-const customerData = [
-    { ssn: "444-44-4444", name: "Bill", age: 35, email: "bill@company.com" },
-    { ssn: "555-55-5555", name: "Donna", age: 32, email: "donna@home.org" }
-  ];
-const dbName = "the_name";
-
-var request = indexedDB.open(dbName, 2);
-request.oncomplete= function(event){
-    alert("All done!");
+if(!window.indexedDB){
+    alert('Not indexdb not allowed here');
 }
-request.onerror = function(event) {
-  // Handle errors.
-};
-request.onupgradeneeded = function(event) {
-  var db = event.target.result;
-
-  // Create an objectStore to hold information about our customers. We're
-  // going to use "ssn" as our key path because it's guaranteed to be
-  // unique - or at least that's what I was told during the kickoff meeting.
-  var objectStore = db.createObjectStore("customers", { autoIncrement: true });
-
-  // Create an index to search customers by name. We may have duplicates
-  // so we can't use a unique index.
-  objectStore.createIndex("confrimName", "name", { unique: false });
-
-  
-
-  // Use transaction oncomplete to make sure the objectStore creation is 
-  // finished before adding data into it.
-  objectStore.transaction.oncomplete = function(event) {
-    // Store values in the newly created objectStore.
-    var customerObjectStore = db.transaction("customers", "readwrite").objectStore("customers");
-    customerData.forEach(function(customer) {
-     var check =  customerObjectStore.add(customer);
-       
-    });
-  };
-  
-};
+else{
+    var request = window.indexedDB.open("MyTestDatabase", 3);
+    request.onerror = function(event){
+        alert('Error in the code')
+    }
+    request.onsuccess = function(event){
+        alert('Set');
+    }
+}
