0abb09330fe222b71705b6f7eebb61581290a1fa
sabelo46 <bellosamuel64@gmail.com>
2018-07-01 00:54:32 +0100

    Confirm caches of currency v2

--- idb/db.js
+++ idb/db.js
@@ -50,30 +50,21 @@ else{
                     // using a cursor
                     var customers=[];
                     var storeCheck = db.transaction("customers").objectStore("customers");
-                    // storeCheck.openCursor().onsuccess = function(event){
-                        // var cursor = event.target.result;
-                        // if(cursor){
-                        //     // alert("Name for SSN " + cursor.key + " is " + cursor.value.name);
-                        //     console.log(cursor.value);
-                        //     customers.push(cursor.value);
-                        //     console.log(customers);
-                        //     cursor.continue();
-                        // }
-                        // else{
-                        //     alert("No more entries!"+ customers);
-                        // }
-                       
-                    // }
-                     //Searching thrrough
-                     
-                        var index = objectStore.index("confirm");
-                        index.openCursor().onsuccess = function(event){
-                            var cursor = event.target.request;
-                            if(cursor){
-                                alert("Name: " + cursor.key + ", SSN: " + cursor.value.ssn + ", email: " + cursor.value.email);
-                                cursor.continue();
-                            }
+                    storeCheck.openCursor().onsuccess = function(event){
+                        var cursor = event.target.result;
+                        if(cursor){
+                            // alert("Name for SSN " + cursor.key + " is " + cursor.value.name);
+                            console.log(cursor.value);
+                            customers.push(cursor.value);
+                            console.log(customers);
+                            cursor.continue();
                         }
+                        else{
+                            alert("No more entries!"+ customers);
+                        }
+                       
+                    }
+                    
                        
                    
                     //To delete code below
