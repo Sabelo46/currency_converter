ef3d6f36da548a7e134efc486825900a588961ff
sabelo46 <bellosamuel64@gmail.com>
2018-06-30 09:48:18 +0100

    Confirm caches of currency v1

--- idb/db.js
+++ idb/db.js
@@ -1,3 +1,9 @@
+spell=[
+    {id:1,title:"Running the check",tool:"Web"},
+    {id:2,title:"Running the tool",tool:"Design"},
+    {id:4,title:"Running the Service",tool:"Service"},
+    {id:3,title:"Running the host",tool:"Host"}
+]
 if(window.indexedDB){
     var request = indexedDB.open('roughDB',1);
     request.onerror = function(event){
@@ -9,6 +15,12 @@ if(window.indexedDB){
     request.onupgradeneeded = function(event){
         var db = event.target.result;
         var objectStore = db.createObjectStore('hello',{keyPath:"id"});
-        objectStore.createIndex("checking","title",{unique:false})
+        objectStore.createIndex("checking","title",{unique:false});
+        objectStore.transaction.oncomplete = function(event){
+            var check = db.transaction(["hello"],"readwrite");
+            var store = check.objectStore('hello');
+            for(let c=0;c< spell.length;c++){
+                store.add(spell[0]);
+            }
+        }
     }
-}
