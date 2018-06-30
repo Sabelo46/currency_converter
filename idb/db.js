b75c1000d8f2e3ce99f93098e4daa97a5b34bf99
sabelo46 <bellosamuel64@gmail.com>
2018-06-30 21:34:26 +0100

    Confirm caches of currency v2

--- idb/db.js
+++ idb/db.js
@@ -4,9 +4,12 @@ if(!window.indexedDB){
 else{
     var request = window.indexedDB.open("MyTestDatabase", 3);
     request.onerror = function(event){
-        alert('Error in the code')
+        alert('Error in the code'+ event.target.errorCode)
     }
     request.onsuccess = function(event){
-        alert('Set');
+        db = event.target.result;
+        request.onupgradeneeded = function(event){
+            alert('Update needed');
+        }
     }
 }
