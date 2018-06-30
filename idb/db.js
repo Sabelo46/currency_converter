1278e5d5df87774b6abda0299d8466662d94a105
sabelo46 <bellosamuel64@gmail.com>
2018-06-30 21:07:16 +0100

    Confirm caches of currency v2

--- idb/db.js
+++ idb/db.js
@@ -1,3 +1,7 @@
+const customerData = [
+    { ssn: "444-44-4444", name: "Bill", age: 35, email: "bill@company.com" },
+    { ssn: "555-55-5555", name: "Donna", age: 32, email: "donna@home.org" }
+  ];
 const dbName = "the_name";
 
 var request = indexedDB.open(dbName, 2);
