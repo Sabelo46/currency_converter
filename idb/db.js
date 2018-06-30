94fef6b5071b168ca5ee48e67ff622373b451dfa
sabelo46 <bellosamuel64@gmail.com>
2018-06-30 10:32:47 +0100

    Confirm caches of currency v2

--- idb/db.js
+++ idb/db.js
@@ -5,28 +5,41 @@ spell=[
     {id:3,title:"Running the host",tool:"Host"}
 ]
 if(window.indexedDB){
-    var request = indexedDB.open('roughDB',1);
-    request.onerror = function(event){
-        console.log(event);
-    }
-    request.onsuccess = function(event){
-        console.log('success');
-    }
-        request.onupgradeneeded = function(event){
-            alert();
-            var db = event.target.result;
-            var objectStore = db.createObjectStore('hello',{keyPath:"id"});
-            objectStore.createIndex("checking","title",{unique:false});
-            objectStore.transaction.oncomplete = function(event){
-                var check = db.transaction(["hello"],"readwrite");
-                var store = check.objectStore('hello');
-                for(let c=0;c< spell.length;c++){
-                    store.add(spell[c]);
-                }
-                request.then(function(){
-                    console.log('Good to go on indexedDB! ');
-                })
-        }   
-    }
-    
+   
+    // request.onerror = function(event){
+    //     console.log(event);
+    // }
+    // request.onsuccess = function(event){
+    //     console.log('success');
+    // }
+    //     request.onupgradeneeded = function(event){
+    //         alert();
+    //         var db = event.target.result;
+    //         var objectStore = db.createObjectStore('hello',{keyPath:"id"});
+    //         objectStore.createIndex("checking","title",{unique:false});
+    //         objectStore.transaction.oncomplete = function(event){
+    //             var check = db.transaction(["hello"],"readwrite");
+    //             var store = check.objectStore('hello');
+    //             for(let c=0;c< spell.length;c++){
+    //                 store.add(spell[c]);
+    //             }
+                
+    //                 console.log('Good to go on indexedDB! ');
+            
+    //     }   
+    // }
+    var dbPromise = indexedDB.open('roughDB',1,function(upGradeDb){
+        var db = event.target.result;
+        var keyValStore = db.createObjectStore('sqi',{keyPath:"id"});
+        keyValStore.createIndex("checking","title",{unique:false});
+    });
+    dbPromise.then(function(response){
+        var tx = response.trasaction('sqi','readwrite');
+        var store = tx.objectStore('sqi');
+        for(let c=0;c< spell.length;c++){
+            store.add(spell[c]);
+        }
+    }).then(function(){
+        console.log('Added to IndexDb successfully');
+    })
 }
