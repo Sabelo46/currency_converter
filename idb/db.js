ab8587cd8a673d788a859bb596a804f9e7b13b08
sabelo46 <bellosamuel64@gmail.com>
2018-07-01 03:00:30 +0100

    Confirm caches of currency v2

--- idb/db.js
+++ idb/db.js
@@ -1,10 +1,32 @@
-self.addEventListener('fetch',function(event){
-    event.respondWith(
-        caches.match(event.request).then(function(response){
-            if(response){
-                console.log('Found Sabelo worker in cache',event.request.url);
-                return response;
-              }
-        })
-    )
-})
+fetch('https://free.currencyconverterapi.com/api/v5/currencies')
+  .then(
+    function(response) {
+      if (response.status !== 200) {
+        return;
+      }
+      // Examine the text in the response
+      response.json().then(function(data) {
+        
+        // console.log(data.results);
+        // let currenciesHolder = data;
+        let currencies = data.results;
+        
+        for(c in currencies){ 
+          let option=document.createElement('option');
+          option.value = `${currencies[c].id}`;  
+          let check = currencies[c].id;
+          if(typeof check === 'undefined'){
+            check ='';
+          }
+        //   option.text =  ` ${check} (${currencies[c].currencyName})`;  
+        //   expect.appendChild(option);
+        //   have.appendChild(option.cloneNode(true));
+        console.log('Okay' + currencies[c].id);
+        }
+
+      });
+    }
+  )
+  .catch(function(err) {
+    console.log('Oops!, err')
+    ;})
