109ab17fe5c7c696b6425061addbbad4b4252b5d
sabelo46 <bellosamuel64@gmail.com>
2018-07-01 02:16:35 +0100

    Confirm caches of currency v2

--- idb/db.js
+++ idb/db.js
@@ -6,12 +6,23 @@ if(window.indexedDB){
     request.onsuccess= function(event){
         console.log('successful');
     }
+    let holder = [];
     request.onupgradeneeded = function(event){
         db = event.target.result;
         let dbStore = db.createObjectStore("results", { keyPath: "id"});
         dbStore.createIndex("arrange","id",{unique: true});
         dbStore.transaction.oncomplete = function(event){
-            alert('cojdejfo');
+                fetch('https://free.currencyconverterapi.com/api/v5/currencies')
+                    .then(function(response){
+                        if (response.status !== 200) {
+                            console.log('Looks like there was a problem. Status Code: ' +
+                            response.status);
+                            return;
+                        }
+                        response.json().then(function(data) {
+                            console.log('okkkk');
+                        });
+                    })
         }
     }
 }
