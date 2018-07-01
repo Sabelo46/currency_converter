0471003a97871037c7847b1a66ff90b0f46dbf9a
sabelo46 <bellosamuel64@gmail.com>
2018-07-01 06:53:12 +0100

    Checking

--- main.js
+++
@@ -1,5 +0,0 @@
-navigator.serviceWorker.register('sw.js').then(function(reg){
-  console.log('Service worker Registered for sabelocurren',reg);
-}).catch(function(err){
-  console.log('Not registered',err);
-})
